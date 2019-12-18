e9afd787480bc5b479843986725ae89c027d2042
Wilbert Lee <wilbert-lee@outlook.com>
2019-12-18 11:21:07 +0800

    Update NSF.cpp

--- src/NSF.cpp
+++ src/NSF.cpp
@@ -220,7 +220,6 @@ DECLARE_COMPONENT_VERSION("Notsofatso", "0.8.6.0",
 "Brad Taylor, Blargg, Kent Hansen, Miyama, Goroh - For the great sound docs.\n"\
 "Wilbert Lee - FamiTracker Support.\n"\
 "\n"\
-"Copyright\n"
 "This program is free software; you can redistribute it and / or modify "\
 "it under the terms of the GNU General Public License as published by "\
 "the Free Software Foundation; either version 2 of the License, or "\
